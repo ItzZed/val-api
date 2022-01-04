c78ac524a8f8115b24c61c3cc7b0285b7e1dc262
ItzZed <bellzach8@gmail.com>
2022-01-03 20:48:52 -0700

    Added api stuff, added constructor and getValContent
    CODE NEEDS A BIG REFACTOR
    pretty much just testing while I try to get this thing working.
    
    Use exampleConfig and rename it to config if you wanna try it in this half broken state :neutral_face:

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // REMINDER THIS IS A PROJECT THAT IS IN DEVELOPMENT
 // THIS IS A TEST APP TO TRY TO GET THE API WORKING
+
+// some code is from valorant.js I will redo those sections but I needed a base template :)
 /*
 module.exports = {
 
@@ -10,10 +12,34 @@ module.exports = {
 };*/
 
 const axios = require('axios');
+const fs = require('fs');
+const API = require('./src/api.js');
 
+// GET API KEY HERE
+const config = require('./config.json');
+const { Console } = require('console');
+const apikey = config.apikey;
+/*
 // test get, replace api key
-axios.get("https://na.api.riotgames.com/val/status/v1/platform-data?api_key=??????????????????").then(resp => {
+axios.get(`https://na.api.riotgames.com/val/status/v1/platform-data?api_key=${apikey}`).then(resp => {
 
 	console.log(resp.data)
 
-});
+});*/
+
+
+const vapi = new API(apikey);
+
+
+try {
+
+
+    vapi.getValContent();
+
+}
+catch(err) {
+
+    console.log(err)
+
+}
+
--- src/api.js
+++ src/api.js
@@ -1,16 +1,57 @@
 "use strict";
 
 const axios = require('axios').default;
-const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+// const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const querystring = require('querystring');
 const tough = require('tough-cookie');
 const url = require('url');
 
+// get those regions
+const regions = require('./regions');
 
-axiosCookieJarSupport(axios);
+// axiosCookieJarSupport(axios);
 
 class API {
 
+    constructor(apikey, region = regions.NorthAmerica, locale = 'en-US') {
+
+        this.apikey = apikey;
+        this.region = region;
+        this.locale = locale;
+        /*this.username = null;
+        this.user_id = null;
+        this.access_token = null;
+        this.entitlements_token = null;
+        this.client_version = 'release-03.00-shipping-22-574489';
+        this.client_platform = {
+            "platformType": "PC",
+            "platformOS": "Windows",
+            "platformOSVersion": "10.0.19042.1.256.64bit",
+            "platformChipset": "Unknown"
+        };*/
+    }
+
     
+    getValContent(region) {
+
+        axios.get(`https://${this.region}.api.riotgames.com/val/content/v1/contents?locale=${this.locale}&api_key=${this.apikey}`).then(resp => {
+
+	        console.log(resp.data)
+
+        });
+
+    }
+
+    getValStatus(region) {
+
+        axios.get(`https://na.api.riotgames.com/val/status/v1/platform-data?api_key=${apikey}`).then(resp => {
+
+	        console.log(resp.data)
+
+    }); 
+
+    }
 
 }
+
+module.exports = API;
